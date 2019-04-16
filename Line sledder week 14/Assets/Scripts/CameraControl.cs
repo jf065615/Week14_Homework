1a3633e925c32ed74ba2fd5ebb6aa75c5283f320
Jack Forschner <jf065615@scc-mmlab-01.local>
2019-04-16 18:44:18 -0400

    ADDED SLED SPRITE AND CAMERA CONTROL

---
+++ Line sledder week 14/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform sledTransform;
+    public Vector3 Offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Offset = transform.position - sledTransform.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = sledTransform.position + Offset;
+    }
+}
